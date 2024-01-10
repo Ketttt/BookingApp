543ffc613f8c6eddbf23944d36988097432e100c
Екатерина Иванова <ei851925@gmail.com>
2024-01-10 17:29:24 +0300

    Initial Commit

---
+++ BookingApp/BookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingAppApp.swift
+//  BookingApp
+//
+//  Created by Katerina Ivanova on 10.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
